aa869efc2c0654aa88e0f8b012d30a0b9025650a
unknown <LocalAdmin@MECH-B107-03.syntraab.mechelen>
2015-08-11 13:47:08 +0200

    oefening arrays

--- Opdracht_arrays_basis_deel2.php
+++ Opdracht_arrays_basis_deel2.php
@@ -15,15 +15,43 @@ $getallen[1]  = 2;
 $getallen[2]  = 3;
 $getallen[3]  = 4;
 $getallen[4]  = 5;
+
+//Maak een tweede array waarin je de getallen 5, 4, 3, 2, 1 in plaatst
+$arr2[0]  = 5;
+$arr2[1]  = 4;
+$arr2[2]  = 3;
+$arr2[3]  = 2;
+$arr2[4]  = 1;
+
+
+
+
 //echo sizeof($getallen);
 
+
+
 $result= 1; //neutraal element voor de vermigvuldiging.
+$onevenGetallen = '';
+
+
 
 for ($i=0; $i < sizeof($getallen) ; $i++) { 
-	
+
+	//Vermenigvuldig alle getallen met elkaar en druk af naar het scherm
 	$result *= $getallen[$i];
 
+	//Druk de oneven getallen af (controle in script, niet zelf selecteren welke je afdrukt)
+	if ($getallen[$i] % 2 !== 0){
+		$onevenGetallen = $onevenGetallen . " " . $getallen[$i];
+	}
+
+	//Tel de getallen uit beide arrays met dezelfde index met elkaar op
+	$arr2[$i] += $getallen[$i];
 }
+
+//resultaat van de optelling...
+var_dump($arr2);
+
 
 
 
@@ -39,6 +67,7 @@ for ($i=0; $i < sizeof($getallen) ; $i++) {
 <body>
 	<h1>Arrays basis, deel 2</h1>
 	<p> Alle getallen in de array met elkaar vermenigvuldigen geeft als resultaat: <?= $result ?> </p>
+	<p> In de array zijn volgende oneven getallen te vinden: <?= $onevenGetallen ?> </p>
 
 
 </body>
