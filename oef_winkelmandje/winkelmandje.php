3c77a635451cb4eac72d16f48e14811c51e704e5
unknown <LocalAdmin@MECH-B107-03.syntraab.mechelen>
2015-08-18 14:10:46 +0200

    winkelmandje (not finished)

--- oef_winkelmandje/winkelmandje.php
+++ oef_winkelmandje/winkelmandje.php
@@ -2,6 +2,9 @@
 
 session_start();
 
+
+////////////// INTIIALISATIE VARIABELEN //////////////////////////////////////
+
 $artikels = array( "appel" => array("appel", "appels"), 
 					"banaan" => array("banaan", "bananen"), 
 					"peer" => array("peer", "peren"),
@@ -10,27 +13,42 @@ $artikels = array( "appel" => array("appel", "appels"),
 					)  ;
 
 
-var_dump($artikels);
+//var_dump($artikels);
+
 
 $winkelmandje = '';
+$artikels_in_mandje = FALSE;
+
+
+
+
+//////////////   Winkelmandje vullen   //////////////////////////////////////////////////////////
 
 //zit er al iets in het winkelmandje?
+
+
+// PROBLEEM wanneer lijst leeg wordt gemaakt.......................................!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 if (isset($_SESSION["winkelmandje"])){
 	foreach ($_SESSION["winkelmandje"] as $key => $value) {
 		$winkelmandje[$key] = $value;
 	}
+	$artikels_in_mandje = TRUE;
 }	
 
+
 //iets toevoegen aan het mandje
-if (isset($_POST["toevoegen"])){
-	
-
+foreach ($artikels as $key => $value) {
+	if (isset($_POST[$key])){
+		$_SESSION["winkelmandje"][] = $key;	
+	//	var_dump($_SESSION);
+	}
+	//var_dump($key . " " . $value[0] . ' ' . $value[1]);
 }
 
-function insert($arr){
-	var_dump($arr);
+if(isset($_POST["maak_leeg"])){
+	$winkelmandje = '';
+	$_SESSION["winkelmandje"] = $winkelmandje;
 }
-
 
 ?>
 
@@ -43,12 +61,26 @@ function insert($arr){
 	<h1>Welkom bij Fruit&Co</h1>
 	<section>
 		<?php foreach ($artikels as $key => $value): ?>
-			<button value="insert(<?=$value?>)"> <?=$value[0]?>  </button> 
+			<form method="post">
+				<button value="<?=$key?>" type="submit"  id="<?=$key?>" name="<?=$key?>" > <?=$key?>   </button> 
+			</form>
 		<?php endforeach ?>
 	</section>
+
 	<aside>
-		
+		<?php if ($artikels_in_mandje) : ?>
+			<?php foreach ($winkelmandje as $volgnummer => $artikelnaam): ?>
+				
+				<p><?=$artikelnaam?></p>
+			<?php endforeach ?>
+			
+		<?php endif ?>
 	</aside>
+
+	<form method="post">
+		<button value="maak_leeg" type="submit" id="maak_leeg" name="maak_leeg"> Maak winkelmanje leeg </button>
+	</form>
+	
 
 </body>
 </html>
